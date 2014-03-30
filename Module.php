88d4cec5a940495f22f49730e977772e41335488
Daniel Strøm <danielss89@gmail.com>
2014-03-30 21:48:44 +0200

    Performance improvement

--- Module.php
+++ Module.php
@@ -10,12 +10,19 @@ class Module
             $routeName       = $e->getRouteMatch()->getMatchedRouteName();
             $config          = $e->getApplication()->getServiceManager()->get('config');
             
-            foreach (array_keys($config['route_layouts']) as $routeRule) {
-                if (fnmatch($routeRule, $routeName, FNM_CASEFOLD)) {
-                    $controller->layout($config['route_layouts'][$routeRule]);
-                    break;
-                }
+            if (isset($config['route_layouts'][$routeName])) {
+                $controller->layout($config['route_layouts'][$routeName]);
+            } else {
+                $rules = array_keys($config['route_layouts']);
+                foreach ($rules as $routeRule) {
+                    if (fnmatch($routeRule, $routeName, FNM_CASEFOLD)) {
+                        $controller->layout($config['route_layouts'][$routeRule]);
+                        break;
+                    }
+                } 
             }
+            
+            
         }, 100);
     }
 }
